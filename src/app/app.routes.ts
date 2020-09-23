6c2cd0cf2fc66eb0fbf96b0902b76abbea4925cb
alexandreserradev <alexandre.serra.dev@gmail.com>
2020-09-23 18:50:07 -0400

    ggg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { OfertaComponent } from './oferta/oferta.component';
 import { Routes } from '@angular/router';
 
 import {HomeComponent } from './home/home.component';
@@ -7,5 +8,6 @@ import { DiversaoComponent } from './diversao/diversao.component';
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurantes', component: RestaurantesComponent },
-  { path: 'diversao', component: DiversaoComponent }
+  { path: 'diversao', component: DiversaoComponent },
+  { path: 'oferta/:id', component: OfertaComponent }
 ];
