4b8129123df03e7cfab5c571a08fef6a6cf19f96
alexandreserradev <alexandre.serra.dev@gmail.com>
2020-09-25 16:27:12 -0400

    ggg

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  endereco = '';
+  numero = '';
+  complemento = '';
+  formaPagamento = '';
+
+  // controles de validacao dos campos
+  enderecoValido: boolean;
+  numeroValido: boolean;
+  complementoValido: boolean;
+  formaPagamentoValido: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  atualizarEndereco(endereco: string): void {
+    this.endereco = endereco;
+
+    if (this.endereco.length > 3) {
+      this.enderecoValido = true;
+    } else {
+      this.enderecoValido = false;
+    }
+
+  }
+
+  atualizarNumero(numero: string): void {
+    this.numero = numero;
+
+    if (this.numero.length > 0) {
+      this.numeroValido = true;
+    } else {
+      this.numeroValido = false;
+    }
+  }
+
+  atualizarComplemento(complemento: string): void {
+    this.complemento = complemento;
+
+    if (this.complemento.length > 0) {
+      this.numeroValido = true;
+    }
+  }
+
+  atualizarFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento;
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true;
+    } else {
+      this.formaPagamentoValido = false;
+    }
   }
 
 }
