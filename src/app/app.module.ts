f3f81707315f1fbb68710969182172ec01b736c7
alexandreserradev <alexandre.serra.dev@gmail.com>
2020-09-25 11:05:42 -0400

    nnn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import '@angular/common/locales/global/pt';
 
 import { ROUTES } from './app.routes';
 
@@ -15,6 +16,8 @@ import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,14 +28,17 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
     RestaurantesComponent,
     OfertaComponent,
     ComoUsarComponent,
-    OndeFicaComponent
+    OndeFicaComponent,
+    DescricaoReduzida // Pipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
